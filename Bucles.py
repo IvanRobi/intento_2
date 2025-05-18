a32bf7c57de97350c04138765c7346b86b5e3643
Ivan Gabriel Robi <ivanro130@gmail.com>
2025-05-18 04:37:51 -0300

    Bucles.py

---
+++ Bucles.py
@@ -0,0 +1,58 @@
+
+
+"""
+
+notas=int(input("Ingrese la Cantidad de Notas: "))
+
+resultado = 0
+
+
+for i in range (notas):
+
+    valor_nota= int(input("Ingrese la nota Obtenida: "))
+    
+    resultado = resultado + valor_nota
+
+promedio = resultado / notas
+
+print (f"El Promedio Obtenido de las Notas es {promedio}")
+
+
+"""
+
+# Obtencion de Numero Primo e impresion de los Mismos de 1 a 21
+
+"""
+
+
+for num in range (2,21):
+
+    primo = 1
+
+    for div in range (2,num):
+
+        if (num % div) == 0:
+
+            primo = 0
+    
+    if primo == 1:
+
+        print (f"{num} ", end="")
+
+print ("\n")
+
+    
+"""
+
+for i in range (2,21):
+
+    print(f"Esta es la Impresion {i}")
+
+    print ("Hola")
+
+    print ("Hola")
+    
+    print ("Hola Mundo")
+
+    
+
